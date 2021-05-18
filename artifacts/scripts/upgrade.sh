8196f2265f6ab4036f433c4584d1522db5aa2725
Babita Bisht <babitabisht2018@gmail.com>
2021-05-18 19:09:10 +0530

    Update upgrade.sh

--- artifacts/scripts/upgrade.sh
+++ artifacts/scripts/upgrade.sh
@@ -11,20 +11,8 @@ if [[ $CC == "mycc" ]]; then
     FOLDER="example_cc"
 fi
 
-if [[ $CC == "mycc2" ]]; then
-echo "inids"
-    FOLDER="module1"
-fi
 
-if [[ $CC == "mycc3" ]]; then
-echo "qw"
-    FOLDER="module8"
-fi
 
-if [[ $CC == "batteryInventoryAndOrderManagement" ]]; then
-echo "batteryInventoryAndOrderManagement"
-    FOLDER="batteryInventoryAndOrderManagement"
-fi
 
 echo "*********************************************************"
 echo "     installing chaincode : $CC version $version  folder $FOLDER    "
